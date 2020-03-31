403b7a44cfa86e2eb0633291bca0aa20498c8e1c
KloderGit <idzhyan.develop@outlook.com>
2020-03-31 11:06:52 +0300

    downgrade .net version to 2.2

--- Fpa.IConfigurationSource.FromService/SeviceConfigurationProvider.cs
+++ Fpa.IConfigurationSource.FromService/SeviceConfigurationProvider.cs
@@ -20,21 +20,21 @@ namespace Fpa.IConfigurationSource.FromService
         {
             using (var client = new WebClient())
             {
-                var response = client.DownloadString(source);
-
                 try
                 {
+                    var response = client.DownloadString(source);
+
                     var value = JsonConvert.DeserializeObject<ConfigDTO>(response);
 
                     if(value != null) Data = value.Params?.Where(p=>p.Active == true).ToDictionary(c => c.Key, c => c.Value);
                 }
                 catch (JsonReaderException ex)
                 {
-                    throw new JsonReaderException("Не удалось преобразовать результат ответа сервера конфигураций к Json объекту");
+                    throw new JsonReaderException("Не удалось преобразовать результат ответа сервера конфигураций к Json объекту", ex);
                 }
                 catch (Exception ex)
                 {
-                    throw new Exception("Не удалось преобразовать полученный результат к Key:Value");
+                    throw new Exception("Не удалось преобразовать полученный результат к Key:Value", ex);
                 }
             }
         }
